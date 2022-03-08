fe1ae890eb0bf9dbdbad2d36bd6dea9cd9a657a2
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-03-08 17:04:51 -0500

    Working on passing down card array to Card.js

--- src/Deck.js
+++ src/Deck.js
@@ -1,18 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const NEW_DECK_URL = "https://deckofcardsapi.com/api/deck/new/";
+const BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 function Deck() {
   const [deckId, setDeckId] = useState(null);
+  const [cards, setCards] = useState([]);
 
   useEffect(function fetchDeckIdWhenMounted() {
     async function fetchDeckId() {
-      const results = await axios.get(NEW_DECK_URL);
+      const results = await axios.get(`${BASE_URL}/new`);
+      setDeckId(results.data.deck_id);
     }
-  })
+    fetchDeckId();
+  }, []);
 
-  return null;
+  async function draw(){
+    const response = await axios.get(`${BASE_URL}/${deckId}/draw/?count=1`);
+    const card = response.data.cards[0];
+    setCards(c => [...c, card]);
+  }
+//From here
+  return (
+  <div className="Deck">
+    <button className="gimmeCard" onClick={draw}>
+
+    </button>
+    <div className="showCard">
+
+      <Card cards = {cards}/>
+    </div>
+    
+  </div>);
 }
 
 export default Deck;
